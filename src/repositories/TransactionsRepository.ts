25576fccd81524f75ef2af81108b5be4c2c84331
Vittor Duarte - Safira <vittor.duarte@safira.local>
2020-06-29 19:18:01 -0300

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,61 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+
+    return this.transactions;
+
+
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const sumIncome: number = this.transactions.filter(item => item.type === 'income').reduce((a, b) => {
+      return a + b.value;
+    }, 0)
+
+    const sumOutcome: number = this.transactions.filter(item => item.type === 'outcome').reduce((a, b) => {
+      return a + b.value;
+    }, 0)
+
+
+    const total: number = sumIncome - sumOutcome;
+
+    const balance = {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: total,
+    }
+
+    return balance;
+
   }
 
-  public create(): Transaction {
-    // TODO
+  private validaOutcome(value: number): boolean {
+
+    const sumIncome: number = this.transactions.filter(item => item.type === 'income').reduce((a, b) => a + b.value, 0);
+    const sumOutcome: number = this.transactions.filter(item => item.type === 'outcome').reduce((a, b) => a + b.value, 0)
+    return  sumIncome - (sumOutcome + value) < 0 ? false : true;
+
+  }
+
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+
+
+
+    const transaction = new Transaction({ title, type, value });
+
+    if (type === 'outcome' && !this.validaOutcome(value)) {
+      throw Error('This transaction can not be completed');
+    }
+
+
+    const sumIncome: number = this.transactions.filter(item => item.type === 'income').reduce((a, b) => {
+      return a + b.value;
+    }, 0)
+
+
+    this.transactions.push(transaction)
+    return transaction;
   }
 }
 
